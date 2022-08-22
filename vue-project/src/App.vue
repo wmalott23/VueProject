90b957be8c1d46663ff9addcff0d5d3e256b86f8
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-08-22 10:58:53 -0700

    Added Axios get request

---
+++ vue-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <GetImages/>
+  </div>
+</template>
+
+<script>
+import GetImages from "./components/GetImages.vue"
+
+export default {
+  name: "app",
+  components: {
+    GetImages
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-project/src/components/GetImages.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h5>
+            {{images}}
+        </h5>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: "get-images",
+    data() {
+        return {
+            images: null
+        }
+    },
+    async created() {
+        const response = await axios.get("http://127.0.0.1:8000/api/images/")
+        this.images = response.data.total
+    },
+}
+</script>
